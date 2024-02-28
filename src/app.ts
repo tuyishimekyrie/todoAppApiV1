b80e21ff27f267c1e6ba5b6165b2a0938bce8945
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-02-28 07:01:17 +0200

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Use the environment variable PORT or default to 3000
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
