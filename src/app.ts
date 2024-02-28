a89a17d0445efbd55eebb4ba580f534fb0c3a817
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-02-28 10:24:55 +0200

    middlewarejson

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT = process.env.PORT || 3000; // Use the environment variable PORT or default to 3000
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Todo app apiV1");
 });
